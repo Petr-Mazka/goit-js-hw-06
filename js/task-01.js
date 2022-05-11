a1b4ca7873c50246945be161865b90948b729d13
Petr Mazka <petr.mazka@gmail.com>
2022-05-11 15:04:06 +0300

    Hw6 the first part
    the first ex. done

---
+++ js/task-01.js
@@ -0,0 +1,29 @@
+"use strict";
+// Задание 1
+
+const categoryList = document.querySelector(".categories");
+const categories = document.querySelectorAll(".item");
+const categoriesArray = Array.from(categories);
+const title = document.querySelectorAll("h2");
+const nameArray = [];
+const titleArray = [...title];
+const categoriesItems = document.querySelectorAll("li.item > ul");
+const catItemsArray = [...categoriesItems];
+
+// Функция для первого задания
+const categoriesCounter = () => {
+    const catArrayLength = categoriesArray.length;
+    console.log(`Number of categories : ${catArrayLength}`);
+
+    for (const item of titleArray) {
+        const name = item.textContent;
+        nameArray.push(name);
+    }
+    
+    
+    for ( let i = 0; i < 3; i += 1) {
+        console.log(`Category : ${nameArray[i]}
+Count : ${catItemsArray[i].children.length}`);
+    }
+}
+categoriesCounter();
