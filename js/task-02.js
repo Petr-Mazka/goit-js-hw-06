8e566fdab7b4b33b5194e16e5d032af7aeb72432
Petr Mazka <petr.mazka@gmail.com>
2022-05-11 16:02:34 +0300

    Hw 6 update
    Ex. 2 completed.

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+
+const addListItem = () => {
+  const ingredientListElement = ingredients.forEach((ingredient) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = ingredient;
+    const listItemClass = listItem.classList.add("item");
+    list.appendChild(listItem);
+  });
+}
+addListItem();
