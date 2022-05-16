b4525d71f08edb6d5645b6946f1bed8325ae76f2
Petr Mazka <petr.mazka@gmail.com>
2022-05-16 12:47:29 +0300

    Hw 6 fix
    Fixed task 2

--- js/task-02.js
+++ js/task-02.js
@@ -13,12 +13,7 @@ const ingredients = [
 const ingredientsList = document.querySelector('#ingredients');
 const ingredientsListLength = ingredients.length;
 
-// Напиши скрипт, который с помошью метода map для каждого элемента массива ingredients:
-
-// Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
-// Добавит название ингредиента как его текстовое содержимое.
-// Добавит элементу класс item.
-// После чего вставит все <li> за одну операцию в список ul.ingredients.
+// Функция для второго задания
 
 const addListItem = () => {
 const ingredientsListItem = ingredients.map(item => {
